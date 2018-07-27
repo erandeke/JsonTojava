32c00010884dc2a3ff8e98196ce3bca5e976ab6c
User <erande.kedar@gmail.com>
2018-07-28 01:12:12 +0530

    Json To Java commit

--- src/com/writejson/CreateJson.java
+++ src/com/writejson/CreateJson.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
